5c35028e113e3496a56303f337ebee28739aba69
Psyfo <21422869@dut4life.ac.za>
2020-07-20 12:47:14 +0200

    Initial simulation setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'khoola-client';
+  item1Show = true;
+  item2Show = false;
+  item3Show = false;
+  item4Show = false;
+  item5Show = false;
+
+  ngOnInit() {}
+
+  click1() {
+    this.item1Show = true;
+    this.item2Show = false;
+    this.item3Show = false;
+    this.item4Show = false;
+    this.item5Show = false;
+  }
+  click2() {
+    this.item1Show = false;
+    this.item2Show = true;
+    this.item3Show = false;
+    this.item4Show = false;
+    this.item5Show = false;
+  }
+  click3() {
+    this.item1Show = false;
+    this.item2Show = false;
+    this.item3Show = true;
+    this.item4Show = false;
+    this.item5Show = false;
+  }
+  click4() {
+    this.item1Show = false;
+    this.item2Show = false;
+    this.item3Show = false;
+    this.item4Show = true;
+    this.item5Show = false;
+  }
+  click5() {
+    this.item1Show = false;
+    this.item2Show = false;
+    this.item3Show = false;
+    this.item4Show = false;
+    this.item5Show = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent, SidebarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatRadioModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
